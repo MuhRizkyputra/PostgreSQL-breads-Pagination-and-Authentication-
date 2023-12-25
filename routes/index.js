26184a5745baf69165f82d036aa995e7e0b35458
Muhammad <muhammadrizkyputraaa@gmail.com>
2023-12-25 15:29:22 +0700

    FE

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+
+router.get('/add', (req, res) => {
+  res.render('add')
+})
 
 module.exports = router;
