52b28a24e8af244b2e57d23f331a89946a0725f1
Muhammad <muhammadrizkyputraaa@gmail.com>
2024-01-04 17:20:11 +0700

    Alert Done

--- routes/index.js
+++ routes/index.js
@@ -16,16 +16,19 @@ module.exports = function (db) {
     try {
       const { email, password } = req.body
       const { rows } = await db.query('SELECT * FROM users WHERE email = $1', [email])
+      
       if (rows.length == 0) {
         new Error(`email doesn't exist`)
         res.redirect('/')
       };
       const passBag = rows[0].password
       const passwordMatch = bcrypt.compareSync(password, passBag)
+      
       if (!passwordMatch) {
         new Error('password wrong')
         res.redirect('/')
       };
+
       req.session.user = passBag
       res.redirect('/users')
     } catch (error) {
@@ -45,6 +48,7 @@ module.exports = function (db) {
       if (password !== repassword) throw new Error(`Password doesn't match`)
       const { rows } = await db.query('SELECT * FROM users WHERE email = $1', [email])
       console.log(rows)
+      
       if (rows.length > 0) throw new Error(`email already exist`)
       const hash = bcrypt.hashSync(password, saltRounds);
       const { rows: users } = await db.query('INSERT INTO users(email, password) VALUES ($1, $2) returning *', [email, hash])
--- routes/users.js
+++ routes/users.js
@@ -10,9 +10,19 @@ module.exports = function (db) {
     res.render('list')
   })
 
-
   router.get('/add', isLoggedIn, (req, res) => {
-    res.render('add')
+    res.render('add', {data: {}})
+  })
+
+  router.post('/add', isLoggedIn, (req, res) => {
+    const title = req.body.title
+    const userId = req.session.user.userid
+    db.query('INSERT INTO todos(title, userid) VALUES ($1, $2)', [title, userId], (err) => {
+      if (err) return res.send(err)
+      res.redirect('/users')
+    })
+    console.log(title)
+    console.log(userId)
   })
 
   router.get('/edit', isLoggedIn, (req, res) => {
