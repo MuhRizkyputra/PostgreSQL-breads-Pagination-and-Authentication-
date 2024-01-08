5bb937f39057a803ed79107621dcf6d9cbade80c
Muhammad <muhammadrizkyputraaa@gmail.com>
2024-01-08 07:56:21 +0700

    Done

--- routes/users.js
+++ routes/users.js
@@ -81,25 +81,25 @@ module.exports = function (db) {
   })
 
 
-  // router.post('/upload', function (req, res) {
-  //   let sampleFile;
-  //   let uploadPath;
+  router.post('/upload', function (req, res) {
+    let sampleFile;
+    let uploadPath;
 
-  //   if (!req.files || Object.keys(req.files).length === 0) {
-  //     return res.status(400).send('No files were uploaded.');
-  //   }
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
 
-  //   sampleFile = req.files.avatar;
-  //   uploadPath = path.join(__dirname, '..', 'public', 'images', sampleFile.name)
-  //   // console.log(uploadPath)
+    sampleFile = req.files.avatar;
+    uploadPath = path.join(__dirname, '..', 'public', 'images', sampleFile.name)
+    // console.log(uploadPath)
 
-  //   sampleFile.mv(uploadPath, function (err) {
-  //     if (err)
-  //       return res.status(500).send(err);
+    sampleFile.mv(uploadPath, function (err) {
+      if (err)
+        return res.status(500).send(err);
 
-  //     res.send('File uploaded!');
-  //   });
-  // });
+      res.send('File uploaded!');
+    });
+  });
 
   return router
 }
